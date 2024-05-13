ec7e577b4092d48929dc4c28643f442bd1dde67a
Sanskar-Chaudhary <sanskarchaudhary2003@gmail.com>
2024-05-13 18:59:51 +0300

    Add CrossOrigin Mechanism

--- src/main/java/ee/mainor/hostel/MainorStudentHostelApplication.java
+++ src/main/java/ee/mainor/hostel/MainorStudentHostelApplication.java
@@ -2,6 +2,9 @@ package ee.mainor.hostel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MainorStudentHostelApplication {
@@ -10,4 +13,19 @@ public class MainorStudentHostelApplication {
 		SpringApplication.run(MainorStudentHostelApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // This allows CORS requests to any URL path
+						.allowedOrigins("http://localhost:3000") // This should match the URL of your frontend server
+						.allowedMethods("GET", "POST", "PUT", "DELETE") // Specify allowed methods
+						.allowedHeaders("*") // Allow all headers
+						.allowCredentials(true);
+			}
+		};
+	}
+
 }
+
